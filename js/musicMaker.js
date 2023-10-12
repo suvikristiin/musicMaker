436a8e81084839d14ae2d586af6705d1260e2f25
suvikristiin <suvi.vehmaanpera@gmail.com>
2023-10-12 19:46:28 +0300

    created drag and drop feature

--- js/musicMaker.js
+++ js/musicMaker.js
@@ -1,4 +1,52 @@
-// Array for mp3 samples, items are object having file source and name
+const createSample = (sample, sampleCollections) => {
+  let id = sampleCollections.children.length;
+  const sampleElement = document.createElement("div");
+  sampleElement.setAttribute("id", "sample" + id++);
+  sampleElement.setAttribute("draggable", true);
+  sampleElement.innerText = sample.name;
+  sampleCollections.appendChild(sampleElement);
+
+  sampleElement.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text/plain", e.target.id);
+  });
+};
+
+const createTrack = (tracksDiv) => {
+  const cloneCounts = {};
+  const index = tracksDiv.children.length / 2;
+
+  const trackDiv = document.createElement("div");
+  const trackSetupDiv = document.createElement("div");
+
+  trackDiv.setAttribute("id", "trackDiv" + index);
+  trackSetupDiv.setAttribute("id", "trackSetupDiv" + index);
+
+  const trackDivHeader = document.createElement("h3");
+  trackDivHeader.innerText = "Track " + (index + 1);
+
+  trackSetupDiv.appendChild(trackDivHeader);
+  tracksDiv.appendChild(trackSetupDiv);
+  tracksDiv.appendChild(trackDiv);
+
+  trackDiv.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  trackDiv.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    const sampleId = e.dataTransfer.getData("text");
+    const originalSample = document.getElementById(sampleId);
+    const clonedSample = originalSample.cloneNode(true);
+
+    cloneCounts[sampleId] = (cloneCounts[sampleId] || 0) + 1;
+
+    const clonedId = `cloned${sampleId}${cloneCounts[sampleId]}`;
+    clonedSample.setAttribute("id", clonedId);
+    clonedSample.classList.add("dropped");
+    trackDiv.appendChild(clonedSample);
+  });
+};
 
 const addInitialSamples = () => {
   const samples = [];
@@ -10,13 +58,11 @@ const addInitialSamples = () => {
   samples.push({ src: "audio/strange-beat.mp3", name: "Strange Beat" });
   samples.push({ src: "audio/violin.mp3", name: "Violin" });
 
+  const sampleCollections = document.getElementById("sampleCollections");
   let id = 0;
+
   samples.forEach((sample) => {
-    console.log(sample.name);
-    const sampleButton = document.createElement("button");
-    sampleButton.setAttribute("data-id", "sample" + id++);
-    sampleButton.innerText = sample.name;
-    addButtons.appendChild(sampleButton);
+    createSample(sample, sampleCollections);
   });
 };
 
@@ -29,17 +75,10 @@ const addInitialTracks = () => {
   tracks.push([]);
 
   const tracksDiv = document.getElementById("tracks");
+  const cloneCounts = {};
 
   for (let i = 0; i < tracks.length; i++) {
-    const trackDiv = document.createElement("div");
-    const trackSetupDiv = document.createElement("div");
-    trackDiv.setAttribute("id", "trackDiv" + i);
-    trackSetupDiv.setAttribute("id", "trackSetupDiv" + i);
-    const trackDivHeader = document.createElement("h3");
-    trackDivHeader.innerText = "Track " + (i + 1);
-    trackSetupDiv.appendChild(trackDivHeader);
-    tracksDiv.appendChild(trackSetupDiv);
-    tracksDiv.appendChild(trackDiv);
+    createTrack(tracksDiv, cloneCounts);
   }
 };
 
